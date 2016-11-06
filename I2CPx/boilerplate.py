be23894c113fd5b8696cc8ab1e60eb4d41e176eb
Luis Argumedo <huicho.argumedo@gmail.com>
2016-11-05 20:06:17 -0600

    boilerplate script

--- I2CPx/boilerplate.py
+++ I2CPx/boilerplate.py
@@ -1,5 +1,5 @@
-with open('aplusb.in', 'r') as input:
+with open('input.in', 'r') as input:
   a, b = [int(x) for x in input.readline().split(' ')]
-  with open('aplusb.out', 'w') as output:
+  with open('output.out', 'w') as output:
     output.write(str(a + b))
     output.write('\n')
---
+++ I2CPx/new.sh
@@ -0,0 +1,3 @@
+mkdir -p $2/$1
+cp boilerplate.py $2/$1/$1.py
+cd $2/$1
